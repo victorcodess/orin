432c30615fa6d4618a7b9dad78e8da23fade4aad
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-06-06 14:05:27 +0100

    fix: suppress hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
+      <body
+        className={`${geistSans.className} antialiased`}
+        suppressHydrationWarning
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
