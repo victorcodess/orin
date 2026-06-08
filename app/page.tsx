55c67c3429b78a515985aebe6618debe9cad51c0
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-06-08 23:29:49 +0100

    chore: restore landing dither

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// import { BayerDitherBackground } from "@/components/orin/bayer-dither-background";
+import { BayerDitherBackground } from "@/components/orin/bayer-dither-background";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { HugeiconsIcon } from "@hugeicons/react";
@@ -7,7 +7,7 @@ import { CircleIcon } from "@hugeicons/core-free-icons";
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-background">
-      {/* <BayerDitherBackground shape="circle" pixelSize={5} color="#ffba08" /> */}
+      <BayerDitherBackground shape="circle" pixelSize={5} color="#ffba08" />
       <nav className="absolute top-8 left-1/2 z-10 h-14 w-120 -translate-x-1/2 rounded-full">
         <div className="bg-secondary flex h-14 w-full items-center justify-between rounded-full px-3.5">
           <Link href="/" className="flex items-center gap-0.75 pl-2">
