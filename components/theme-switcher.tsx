9ae6fa8addb94dbdecb37c0e7e03e5a7a79b87c8
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-06-16 21:48:53 +0100

    feat: make landing page mobile responsive

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -6,8 +6,9 @@ import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { HugeiconsIcon } from "@hugeicons/react";
+import { cn } from "@/lib/utils";
 
-const ThemeSwitcher = () => {
+const ThemeSwitcher = ({className}: {className?: string}) => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
 
@@ -22,17 +23,20 @@ const ThemeSwitcher = () => {
 
   const isDark = resolvedTheme === "dark";
 
+  const iconClassName = "text-muted-foreground/90 rounded-full";
+
   return (
     <Button
       variant="ghost"
-      size="icon-sm"
+      size="icon"
       aria-label={isDark ? "Switch to light mode" : "Switch to dark mode"}
       onClick={() => setTheme(isDark ? "light" : "dark")}
+      className={cn(className, "hover:bg-accent hover:dark:bg-muted")}
     >
       {!isDark ? (
-        <HugeiconsIcon icon={Moon02Icon} strokeWidth={2} className={"size-4.5 text-primary/90"} />
+        <HugeiconsIcon icon={Moon02Icon} strokeWidth={2} className={cn(iconClassName, "size-4.5")} />
       ) : (
-        <HugeiconsIcon icon={Sun01Icon} strokeWidth={2} className={"size-5.5 text-primary/90"} />
+        <HugeiconsIcon icon={Sun01Icon} strokeWidth={2} className={cn(iconClassName, "size-5.5")} />
       )}
     </Button>
   );
